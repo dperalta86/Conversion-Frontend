a4034c44232c5b25944b678f54674c854a593e1d
Daniel Peralta <dp25443@gmail.com>
2024-08-24 21:19:48 -0300

    Prueba de comunicacion interna

--- src/components/ConversionForm.js
+++ src/components/ConversionForm.js
@@ -11,7 +11,7 @@ const ConversionForm = () => {
 
     const handleConvert = async () => {
         try {
-            const response = await fetch('https://conversornumerico-production.up.railway.app', {
+            const response = await fetch('https://conversornumerico-production.up.railway.app/conversion', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
