8800c0e7912562b9a9e353d9311c9eba0724d4ba
Daniel Peralta <dp25443@gmail.com>
2024-08-26 12:07:12 -0300

    cambio url backend

--- src/components/ConversionForm.js
+++ src/components/ConversionForm.js
@@ -11,7 +11,7 @@ const ConversionForm = () => {
 
     const handleConvert = async () => {
         try {
-            const response = await fetch('https://conversornumerico-production.up.railway.app/conversion', {
+            const response = await fetch('https://conversor-springboot.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
